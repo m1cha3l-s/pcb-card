c77c41b4a4bd477decffb2db9a3d2bca760c2def
m1cha3l-s <254994195+m1cha3l-s@users.noreply.github.com>
2026-04-16 10:13:39 +0200

    changed button2 to fill matrix based on temp

--- Firmware/animation_vibration/animation_vibration.ino
+++ Firmware/animation_vibration/animation_vibration.ino
@@ -1,6 +1,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_BME280 bme;
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(6, 6, D9,   
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -15,9 +20,10 @@ void setup() {
   pinMode(1, INPUT);
   pinMode(3, OUTPUT);
 
+  bme.begin(0x76);
+  float temp = bme.readTemperature();
+
   matrix.begin();
-  matrix.setTextColor(matrix.Color(175, 10, 180));
-  matrix.print("m1cha3l");
   matrix.setBrightness(0);
   matrix.show();
 }
@@ -25,15 +31,29 @@ void setup() {
 void loop() {
   button1 = digitalRead(0);
   button2 = digitalRead(1);
+  float temp = bme.readTemperature();
   if (button1 == HIGH) {
     digitalWrite(3, HIGH);
     delay(500);
     digitalWrite(3, LOW);
   }
   else if (button2 == HIGH) {
-    matrix.setBrightness(100);
-    delay(500);
-    matrix.setBrightness(0);
+    if (temp <= 20) {
+      matrix.fillScreen(matrix.Color(0, 255, 0));
+      matrix.setBrightness(255);
+      matrix.show();
+      delay(1500);
+      matrix.setBrightness(0);
+      matrix.show();
+    } else {
+      matrix.fillScreen(matrix.Color(255, 0, 0));
+      matrix.setBrightness(255);
+      matrix.show();
+      delay(1500);
+      matrix.setBrightness(0);
+      matrix.show();
+
+    }
   }
 
 }
